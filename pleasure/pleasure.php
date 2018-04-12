a40f6b460a3f3b5efab9bcb4a3a9431c43442cab
byunjungchan <soyo0404@naver.com>
2018-04-12 20:22:52 +0900

    1page(last)
    css, html 끝

--- pleasure/pleasure.php
+++ pleasure/pleasure.php
@@ -17,31 +17,98 @@
        </div>
         <div class="section1">
             <div class="content">
-                <div class="list1"></div>
-                <div class="list2"></div>
-                <div class="list3"></div>
+                <div class="list1">
+                    <img src="./img/div_01_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list2">
+                    <img src="./img/div_02_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list3">
+                    <img src="./img/div_03_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
             </div>
         </div>
         
         
         <div class="section2">
             <div class="content">
-                <div class="list1"></div>
-                <div class="list2"></div>
-                <div class="list3"></div>
-                <div class="list4"></div>
-                <div class="list5"></div>
-                <div class="list6"></div>
-                <div class="list7"></div>
+                <div class="list1">
+                    <img src="./img/div_04_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list2">
+                    <img src="./img/div_05_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list3">
+                    <img src="./img/div_06_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list4">
+                    <img src="./img/div_07_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list5">
+                    <img src="./img/div_08_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list6">
+                    <img src="./img/div_09_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list7">
+                    <img src="./img/div_10_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
             </div>
         </div>
         
         
         <div class="section3">
             <div class="content">
-                <div class="list1"></div>
-                <div class="list2"></div>
-                <div class="list3"></div>
+                <div class="list1"><img src="./img/div_11_img.jpg" alt="">
+                </div>
+                <div class="list2"><img src="./img/div_11_img_02.jpg" alt="">
+                </div>
+                <div class="list3">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
             </div>
             
         </div>
@@ -49,9 +116,27 @@
         
         <div class="section4">
             <div class="content">
-                <div class="list1"></div>
-                <div class="list2"></div>
-                <div class="list3"></div>
+                <div class="list1">
+                    <img src="./img/div_12_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list2">
+                    <img src="./img/div_13_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
+                <div class="list3">
+                    <img src="./img/div_14_img.jpg" alt="">
+                    <h1>콘서트장입니다.</h1>
+                    <p>쇼핑 그 이상의 즐거움<br>
+트렌디한 쇼핑과 63빌딩, 여의도의 다양한<br>
+즐길 거리를 제공하는 One-stop Tour Destination</p>
+                </div>
             </div>
             
         </div>
